7cffcdb4b6ac327a792f06e30dae29b93f4f86c9
Roberto Candelario <r-jc@live.com>
2024-08-11 16:30:10 -0400

    Initial Commit

---
+++ Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StacksTests/StacksTests.swift
@@ -0,0 +1,17 @@
+//
+//  StacksTests.swift
+//  StacksTests
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import Testing
+@testable import Stacks
+
+struct StacksTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
